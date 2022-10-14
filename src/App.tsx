feb683dbf64b6e1ad71ff1ca67c18507bd6b938a
KSushanth07 <sushanthkummitha@outlook.com>
2022-10-14 19:38:36 +0530

    google oauth

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import { GoogleOAuth } from './GoogleOAuth';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>A simple OAuth application by KS</h1>
+        <GoogleOAuth />
       </header>
     </div>
   );
